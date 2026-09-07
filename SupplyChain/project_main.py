947a03a88d0191ca3544148d00e2026676d0caf9
HKenzoKimura <henriquekkimura@gmail.com>
2026-09-06 21:46:20 -0300

    Add files via upload

---
+++ SupplyChain/project_main.py
@@ -0,0 +1,35 @@
+import internal_utils
+import pandas as pd
+import requests
+import logging as log
+import os
+import pathlib
+
+
+
+log.error('EXECUTADO APLICAÇÃO')
+a = 1
+b = 2
+c = 3
+e = 10
+
+def calcular(a, b, c, e):
+    a = a
+    b = b
+    c = c
+    e = e
+
+    if a == 0:
+        log.error('Contém valor igual a 0 da variavel: {a}')
+    elif b == 0:
+        log.error('Contém valor igual a 0 da variavel: {b}')
+    elif c == 0:
+        log.error('Contém valor igual a 0 da variavel: {c}')
+    elif e == 0:
+        log.error('Contém valor igual a 0 da variavel: {e}')
+
+calcular(a, b, c, e)
+resultado = internal_utils.process_data({"nome": "joao", "valor": 42})
+print("Resultado:", resultado)
+
+log.error('FIM EXECUÇÃO DA APLICAÇÃO')
